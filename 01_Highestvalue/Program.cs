af6cf15cd9928aeeb1be1ea54e7f60b9954328df
Thomas Jens Nielsen Thorogood <thom562r@mail.hansenberg.dk>
2024-01-14 19:47:53 +0100

    getting 01 and 02

---
+++ 01_Highestvalue/Program.cs
@@ -0,0 +1,53 @@
+﻿int[] array = new int[10];
+Random rnd = new Random();
+string yessir;
+int numberofattempts = 1;
+Console.Clear();
+Console.WriteLine("Please Input a number with less than 4 digits.");
+while (true)
+{
+    yessir = Console.ReadLine();
+    if (yessir.Length < 4)
+    {
+        break;
+    }
+    else
+    {  
+        Console.Clear();
+        Console.WriteLine("That input is not below 4 digits! Try again."+"("+numberofattempts+")");
+        numberofattempts++;
+    }
+}
+Console.Clear();
+Console.WriteLine("This was your input: " + yessir);
+Console.Write("These are the random numbers: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 9);
+
+    Console.Write(array[i] + " ");
+}
+int substring1 = Convert.ToInt32(yessir.Substring(0, 1));
+int substring2 = Convert.ToInt32(yessir.Substring(1, 1));
+int substring3 = Convert.ToInt32(yessir.Substring(2, 1));
+String numb;
+if (array[substring1 - 1] > substring1)
+{
+    numb = Convert.ToString(array[substring1 - 1]);
+    yessir = yessir.Remove(0, 1).Insert(0, numb);
+}
+
+if (array[substring2 - 1] > substring2)
+{
+    numb = Convert.ToString(array[substring2 - 1]);
+    yessir = yessir.Remove(1, 1).Insert(1, numb);
+}
+
+if (array[substring3 - 1] > substring3)
+{
+    numb = Convert.ToString(array[substring3 - 1]);
+    yessir = yessir.Remove(2, 1).Insert(2, numb);
+}
+Console.WriteLine("\n--------------------------------------------");
+Console.WriteLine("The highest possible number is: " + yessir);
+Console.ReadKey();
---
+++ 02_wordcracker/Program.cs
@@ -0,0 +1,62 @@
+﻿
+int length = 4;
+Console.Clear();
+Console.WriteLine($"Enter a word with less than {length} letters.");
+
+int z = 1;
+Random rnd = new Random();
+string random = "";
+char[] bogstaver =
+{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+string input;
+int n = 0;
+
+while (true)
+{
+    input = Console.ReadLine();
+    bool dad = true;
+
+    foreach (char bogstav in input)
+    {
+        if (!bogstaver.Contains(bogstav))
+        {
+            dad = false;
+            break;
+        }
+    }
+
+    if (input.Length < length + 1 && dad)
+    {
+        break;
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine($"Input was not a word with less than {length} letters. Try again({z})");
+        z++;
+    }
+
+}
+DateTime before = DateTime.Now;
+DateTime after;
+while (true)
+{
+    random = "";
+    for (int i = 0; i < input.Length; i++)
+    {
+        char scooby = bogstaver[rnd.Next(25)];
+        random += scooby;
+    }
+    n++;
+    if (input == random)
+    {
+        after = DateTime.Now;
+        break;
+    }
+    Console.Write(random + " ");
+}
+TimeSpan elapsed = after - before;
+Console.WriteLine($"{random}\n\nAfter {n} attempts and " + Math.Round(elapsed.TotalSeconds, 2) + " seconds we found your input.");
+
+Console.WriteLine("That's " + Math.Round(n / elapsed.TotalSeconds, 0) + " attemps per second.\n");
+Console.ReadKey();
