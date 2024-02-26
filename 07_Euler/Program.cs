89528edd674e85b639607d40d34f1fb01f38aa17
Thomas Jens Nielsen Thorogood <thom562r@mail.hansenberg.dk>
2024-02-26 21:52:03 +0100

    jik

--- 07_Euler/Program.cs
+++ 07_Euler/Program.cs
@@ -1,33 +1,33 @@
 ﻿// https://projecteuler.net/archives
 
 // ID 1
-
 /*
-int sum = 0;
 
-for (int i = 1; i < 1000; i++)
+ int sum = 0;
+
+for(int i = 1; i < 1000; i++)
 {
     int same = 0;
-    if (i % 3 == 0)
+    if(i % 3 == 0)
     {
         same += i;
         Console.Write(i + " ");
         sum += i;
     }
-    if (i % 5 == 0)
+    if(i % 5 == 0)
     {
-        if (same == i)
-        { }
+        if(same == i)
+        {}
         else
         {
-            Console.Write(i + " ");
-            sum += i;
+        Console.Write(i + " ");
+         sum += i;
         }
     }
 }
 Console.WriteLine($"\nThe sum is {sum}");
-
 */
+
 // ID 2
 /*
 int f1 = 1;
@@ -142,40 +142,40 @@ Console.WriteLine(answer);
 */
 
 //ID 7
-
+/*
 
 int count = 0;
 int term = 0;
 for (int i = 2; i < 40000000; i++)
 {
-    for (int j = 2; j < i; j++)
+    for(int j = 2; j<i; j++)
     {
-        if (i % j == 0)
+        if(i % j == 0)
         {
             count++;
         }
-        if (count > 1)
+        if(count > 1)
         {
-            break;
+          break;  
         }
-
+        
     }
 
-    if (count < 1)
+    if(count < 1)
     {
         Console.WriteLine(i);
         term += 1;
 
     }
     count = 0;
-    if (term == 10001)
+    if(term == 10001)
     {
         Console.WriteLine($"The 10001 term is {i}");
-        break;
+       break; 
     }
 
 }
-
+*/
 // ID 8
 /*
 String digits = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
@@ -459,7 +459,7 @@ for (double i = 0; i <= term; i++)
         {
             num = num * (i - j + 1) / j;
         }
-        if (num > largestnumber)
+        if(num>largestnumber)
         {
             largestnumber = num;
         }
@@ -473,8 +473,7 @@ for (double i = 0; i <= term; i++)
 }
 Console.WriteLine("---------------------------");
 Console.WriteLine(largestnumber);
-
-// ID 16 (unfinished)
+*/
 /*
 double sum = 1;
 double digitsum = 0;
@@ -487,6 +486,8 @@ string dada = Convert.ToString(sum);
 Console.WriteLine(dada);
 String substring = dada.Substring(0,20);
 Console.WriteLine(substring);
+*/
+/*
 Console.WriteLine(sum);
 for(double i = 1; i <=319; i++)
 {
@@ -500,7 +501,93 @@ for(double i = 1; i <=319; i++)
 
 Console.WriteLine(digitsum);
 */
+/*
+int product = 1;
 
-//ID 17
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{i} * {product} = {i*product}");
+    product *= i;
+    
+
+
+}
+*/
+// ID 20
+/*
+using System.Numerics;
+BigInteger sum = 1;
+int digitcount = 0;
+int digitsum = 0;
+
+for (int i = 100; i > 0; i--)
+{
+    sum *= i;
+}
+string numstring = Convert.ToString(sum);
+Console.WriteLine(numstring);
+
+foreach (char a in numstring)
+{
+    Console.WriteLine(numstring.Substring(digitcount,1));
+    digitsum += Convert.ToInt16(numstring.Substring(digitcount,1));
+    digitcount++;
+}
+Console.WriteLine(digitsum);
+*/
+// ID 25
+/*
+using System.Numerics;
+
+BigInteger fibnum = 3;
+BigInteger fibnum2 = 5;
+BigInteger sum;
+int digitnum = 0;
+int iteration = 5;
+
+
+
+for (int i = 0; digitnum != 1000; i++)
+{
+    digitnum = 0;
+    sum = fibnum + fibnum2;
+    fibnum = fibnum2;
+    fibnum2 = sum;
+
+    iteration++;
+    string fibstring = Convert.ToString(sum);
+    foreach (char a in fibstring)
+    {
+        digitnum++;
+    }
+}
+
+Console.WriteLine(iteration);
+*/
+// ID 29
+/*
+using System.Numerics;
+int number = 0;
+List<string> numbers = new List<string>();
+
+
+for (double i = 2; i <= 100; i++)
+{
+    for (double power = 2; power <= 100; power++)
+    {
+        numbers.Add(Convert.ToString(Math.Pow(i,power)));
+        number++;
+
+
+       
+    }
+}
+
+// Hashset har acceptere kun unikke numre.
+var uniquenumbers = new HashSet<string>(numbers);
+
+int count = uniquenumbers.Count; 
+Console.WriteLine(count);
+*/
 
 
