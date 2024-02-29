c228c31ba8301230092c682d8a9ad99778f3fd18
Thomas Jens Nielsen Thorogood <thom562r@mail.hansenberg.dk>
2024-02-29 21:43:10 +0100

    methods

---
+++ 10_methods/Program.cs
@@ -0,0 +1,16 @@
+﻿
+public class Program {
+
+
+public static void Main(){
+    PrintPerson();
+    PrintPerson("Jason Doe", 18);
+    PrintPerson("Jaon Doe", 82);
+
+}
+
+public static void PrintPerson(string name = "adad", int age = 16){
+    Console.WriteLine($"{name} is {age} years old");
+}
+
+}
